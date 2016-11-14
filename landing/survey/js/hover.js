fd3e839d77df2159443a149549360d847be3127e
Rafael Mello Campanari <rafael@veridu.com>
2016-11-14 09:00:28 -0200

    Thank you form update

---
+++ landing/survey/js/hover.js
@@ -0,0 +1,340 @@
+$( "#go" ).click(function() {
+  $( "#block" ).animate({
+    width: "70%",
+    opacity: 0.4,
+    marginLeft: "0.6in",
+    fontSize: "3em",
+    borderWidth: "10px"
+  }, 1500 );
+});
+
+<!-- Video hover -->
+
+$(document).ready(function(){
+    $("#flip").hover(function(){
+        $("#panel").slideDown("slow");
+    });
+});
+
+<!-- Demos hover -->
+
+$(document).ready(function(){
+    $(".story-demo-hover-1").on({
+        mouseenter: function(){
+            $(".story-demo-text-1").css("height", "23%");
+            $(".story-demo-text-1").css("background-color", "#f2f2f2");
+        },
+        mouseleave: function(){
+            $(".story-demo-text-1").css("height", "23%");
+            $(".story-demo-text-1").css("background-color", "#fff");
+        },
+        click: function(){
+        }
+    });
+});
+
+$(document).ready(function(){
+    $(".story-demo-hover-2").on({
+        mouseenter: function(){
+            $(".story-demo-text-2").css("height", "23%");
+            $(".story-demo-text-2").css("background-color", "#f2f2f2");
+        },
+        mouseleave: function(){
+            $(".story-demo-text-2").css("height", "23%");
+            $(".story-demo-text-2").css("background-color", "#fff");
+        },
+        click: function(){
+        }
+    });
+});
+
+$(document).ready(function(){
+    $(".story-demo-hover-3").on({
+        mouseenter: function(){
+            $(".story-demo-text-3").css("height", "23%");
+            $(".story-demo-text-3").css("background-color", "#f2f2f2");
+        },
+        mouseleave: function(){
+            $(".story-demo-text-3").css("height", "23%");
+            $(".story-demo-text-3").css("background-color", "#fff");
+        },
+        click: function(){
+        }
+    });
+});
+
+<!-- show contact -->
+
+$(document).ready(function(){
+    $(".div333331").hide();
+    $(".button-contac33t").click(function(){
+        $(".d22iv1").fadeToggle("slow");
+    }, "fast"); 
+});
+<!-- News hover -->
+
+$(document).ready(function(){
+    $(".story-news-1").on({
+        mouseenter: function(){
+            $(".news-line-1").animate({
+                height: '40px'
+            }, 100);
+        },
+        mouseleave: function(){
+             $(".news-line-1").animate({
+                height: '2px'
+            }, 100);
+        },
+    });
+});
+
+$(document).ready(function(){
+    $(".story-news-2").on({
+        mouseenter: function(){
+            $(".news-line-2").animate({
+                height: '40px'
+            }, 100);
+        },
+        mouseleave: function(){
+             $(".news-line-2").animate({
+                height: '2px'
+            }, 100);
+        },
+    });
+});
+
+$(document).ready(function(){
+    $(".story-news-3").on({
+        mouseenter: function(){
+            $(".news-line-3").animate({
+                height: '40px'
+            }, 100);
+        },
+        mouseleave: function(){
+             $(".news-line-3").animate({
+                height: '2px'
+            }, 100);
+        },
+    });
+});
+
+$(document).ready(function(){
+    $(".story-news-4").on({
+        mouseenter: function(){
+            $(".news-line-4").animate({
+                height: '40px'
+            }, 100);
+        },
+        mouseleave: function(){
+             $(".news-line-4").animate({
+                height: '2px'
+            }, 100);
+        },
+    });
+});
+
+$(document).ready(function(){
+    $(".story-news-5").on({
+        mouseenter: function(){
+            $(".news-line-5").animate({
+                height: '40px'
+            }, 100);
+        },
+        mouseleave: function(){
+             $(".news-line-5").animate({
+                height: '2px'
+            }, 100);
+        },
+    });
+});
+
+<!-- Thought Leadership lines hover -->
+
+$(document).ready(function(){
+    $(".story-thought-leadership-1").on({
+        mouseenter: function(){
+            $(".thought-leadership-line-1").animate({
+                height: '40px'
+            }, 100);
+        },
+        mouseleave: function(){
+            $(".thought-leadership-line-1").animate({
+                height: '2px'
+            }, 100);
+        },
+    });
+});
+
+$(document).ready(function(){
+    $(".story-thought-leadership-2").on({
+        mouseenter: function(){
+            $(".thought-leadership-line-2").animate({
+                height: '40px'
+            }, 100);
+        },
+        mouseleave: function(){
+            $(".thought-leadership-line-2").animate({
+                height: '2px'
+            }, 100);
+        },
+    });
+});
+
+<!-- Video hover -->
+
+$(document).ready(function(){
+    $(".iam-video-hover").on({
+        mouseenter: function(){
+            $(".iam-video-text-hover").css("background-color", "#D8D8D8");
+        },
+        mouseleave: function(){    
+            $(".iam-video-text-hover").css("background-color", "#fff");
+        },
+        click: function(){
+        }
+    });
+});
+
+$(document).ready(function(){
+    $(".intro-video-hover").on({
+        mouseenter: function(){
+            $(".intro-video-text-hover").css("background-color", "#D8D8D8");
+        },
+        mouseleave: function(){    
+            $(".intro-video-text-hover").css("background-color", "#fff");
+        },
+        click: function(){
+        }
+    });
+});
+
+$(document).ready(function(){
+    $(".philip-video-hover").on({
+        mouseenter: function(){
+            $(".philip-video-text-hover").css("background-color", "#D8D8D8");
+
+            
+        },
+        mouseleave: function(){    
+            $(".philip-video-text-hover").css("background-color", "#fff");
+        },
+        click: function(){
+        }
+    });
+});
+
+$(document).ready(function(){
+    $(".ecommerce-video-hover").on({
+        mouseenter: function(){
+            $(".ecommerce-video-text-hover").css("background-color", "#D8D8D8");
+
+            
+        },
+        mouseleave: function(){    
+            $(".ecommerce-video-text-hover").css("background-color", "#fff");
+        },
+        click: function(){
+        }
+    });
+});
+
+$(document).ready(function(){
+    $(".worldpay-video-hover").on({
+        mouseenter: function(){
+            $(".story-video-text-hover").css("background-color", "#D8D8D8");
+
+            
+        },
+        mouseleave: function(){
+            $(".story-video-text-hover").css("background-color", "#fff");
+        },
+        click: function(){
+        }
+    });
+});
+
+$(document).ready(function(){
+    $(".report-video-hover").on({
+        mouseenter: function(){
+            $(".story-report-text-hover").css("background-color", "#D8D8D8");
+
+            
+        },
+        mouseleave: function(){
+            $(".story-report-text-hover").css("background-color", "#f1f1f1");
+        },
+        click: function(){
+        }
+    });
+});
+
+$(document).ready(function(){
+    $(".accept-video-hover").on({
+        mouseenter: function(){
+            $(".accept-video-text-hover").css("background-color", "#D8D8D8");
+
+            
+        },
+        mouseleave: function(){
+            $(".accept-video-text-hover").css("background-color", "#fff");
+        },
+        click: function(){
+        }
+    });
+});
+
+$(document).ready(function(){
+    $(".about-video-hover").on({
+        mouseenter: function(){
+            $(".about-video-text-hover").css("background-color", "#D8D8D8");
+
+            
+        },
+        mouseleave: function(){
+            $(".about-video-text-hover").css("background-color", "#fff");
+        },
+        click: function(){
+        }
+    });
+});
+
+<!-- Product Cards hover -->
+
+$(document).ready(function(){
+    $(".onboarding-product").on({
+        mouseenter: function(){
+            $(".onboarding-line").css("height", "4px");            
+        },
+        mouseleave: function(){
+            $(".onboarding-line").css("height", "2px");
+        },
+        click: function(){
+        }
+    });
+});
+
+$(document).ready(function(){
+    $(".activity-product").on({
+        mouseenter: function(){
+            $(".activity-line").css("height", "4px");            
+        },
+        mouseleave: function(){
+            $(".activity-line").css("height", "2px");
+        },
+        click: function(){
+        }
+    });
+});
+
+$(document).ready(function(){
+    $(".transaction-product").on({
+        mouseenter: function(){
+            $(".transaction-line").css("height", "4px");            
+        },
+        mouseleave: function(){
+            $(".transaction-line").css("height", "2px");
+        },
+        click: function(){
+        }
+    });
+});
